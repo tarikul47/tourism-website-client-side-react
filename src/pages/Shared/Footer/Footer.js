999127e8a73720e0d8d7c1aec9673ed4d44d0440
Tarikul Islam <tarikul47@gmail.com>
2021-10-30 00:04:28 +0600

    Footer design fix

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>Footer</h2>
+  return (
+    // footer
+    <footer className="bottom-footer-area">
+      <div className="container">
+        <div className="row py-3">
+          <div className="col-lg-6 mx-auto">
+            <p className="copywrite-text">
+              Copyright ©2021 |{" "}
+              <Link to="https://onlytarikul.com" target="_blank">
+                Tarikul Islam
+              </Link>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
