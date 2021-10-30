b16d572a871ae046d96204ffd4fec8aab679195d
Tarikul Islam <tarikul47@gmail.com>
2021-10-30 16:45:37 +0600

    Add New Servcie Form added

--- src/pages/Private/AddService/AddService.js
+++ src/pages/Private/AddService/AddService.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./AddService.css";
 
 const AddService = () => {
-    return (
-        <div>
-            <h2>Add New Service</h2>
+  const { register, handleSubmit,reset } = useForm();
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:5000/services", data).then((res) => {
+        if(res.data.insertedId){
+            alert('Service added successfully');
+            reset();
+        }
+    });
+  };
+
+  return (
+    <section className="form-signin">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 mx-auto">
+            <h3>Please Add New Service</h3>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="d-flex flex-column post-form"
+            >
+              <input
+                {...register("sName", { required: true })}
+                placeholder="Servcie Name"
+              />
+              <textarea
+                {...register("sdescription", { required: true })}
+                placeholder="Short description"
+              />
+              <input
+                type="number"
+                {...register("price", { required: true })}
+                placeholder="Price"
+              />
+              <input
+                type="url"
+                {...register("sImage", { required: true })}
+                placeholder="Image Url"
+              />
+              <input type="submit" />
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default AddService;
